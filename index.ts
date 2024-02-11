1323a17a84324415fde7b6945c6a75c969961d69
bryant tunbutr <bryantt23@users.noreply.github.com>
2024-02-11 04:42:30 -0800

    get entries

--- index.ts
+++ index.ts
@@ -1,4 +1,6 @@
 import express from 'express';
+import diagnosisService from './services/diagnosisService';
+
 const app = express();
 app.use(express.json());
 
@@ -7,6 +9,10 @@ const PORT = 3001;
 app.get('/api/ping', (_req, res) => {
     console.log('someone pinged here');
     res.send('pong');
+});
+
+app.get('/api/diagnoses', (_req, res) => {
+    res.send(diagnosisService.getEntries())
 });
 
 app.listen(PORT, () => {
